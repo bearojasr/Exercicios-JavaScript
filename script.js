68f8701b9e117898cd02375501b9888f6b98d527
Beatriz Rojas <beatrizadrianarojas@gmail.com>
2020-10-28 20:05:32 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = prompt('Informe seu nome:')
+let nascimento = prompt('Informe sua data de nascimento:')
+let idade = prompt('Informe seu idade:')
+let profissao = prompt('Informe seu profissao:')
+let Nacionalidade = prompt ('Informe seu nacionalidade:')
+
+
+alert("Bienvenida", +nome); 
+
+
+function enviarFormulario(){
+    console.log('Enviando formulario...');
+}
